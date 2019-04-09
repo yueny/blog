ef06f76b2d1682e1578beecfd6a12633f87e2a24
yuany <yuany@aicaigroup.com>
2019-04-09 14:41:34 +0800

    md

---
+++ src/main/java/com/mtons/mblog/base/storage/impl/AbstractStorage.java
@@ -0,0 +1,65 @@
+/*
++--------------------------------------------------------------------------
+|   Mblog [#RELEASE_VERSION#]
+|   ========================================
+|   Copyright (c) 2014, 2019 mtons. All Rights Reserved
+|   http://www.mtons.com
+|
++---------------------------------------------------------------------------
+*/
+package com.mtons.mblog.base.storage.impl;
+
+import com.mtons.mblog.base.lang.MtonsException;
+import com.mtons.mblog.base.storage.Storage;
+import com.mtons.mblog.base.utils.FileKit;
+import com.mtons.mblog.base.utils.FilePathUtils;
+import com.mtons.mblog.base.utils.ImageUtils;
+import com.mtons.mblog.config.SiteOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author langhsu
+ * @since 3.0
+ */
+@Slf4j
+public abstract class AbstractStorage implements Storage {
+    @Autowired
+    protected SiteOptions options;
+
+    protected void validateFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new MtonsException("文件不能为空");
+        }
+
+        if (!FileKit.checkFileType(file.getOriginalFilename())) {
+            throw new MtonsException("文件格式不支持");
+        }
+    }
+
+    @Override
+    public String store(MultipartFile file, String basePath) throws Exception {
+        validateFile(file);
+        return writeToStore(file.getBytes(), basePath, file.getOriginalFilename());
+    }
+
+    @Override
+    public String storeScale(MultipartFile file, String basePath, int maxWidth) throws Exception {
+        validateFile(file);
+        byte[] bytes = ImageUtils.scaleByWidth(file, maxWidth);
+        return writeToStore(bytes, basePath, file.getOriginalFilename());
+    }
+
+    @Override
+    public String storeScale(MultipartFile file, String basePath, int width, int height) throws Exception {
+        validateFile(file);
+        byte[] bytes = ImageUtils.screenshot(file, width, height);
+        return writeToStore(bytes, basePath, file.getOriginalFilename());
+    }
+
+    public String writeToStore(byte[] bytes, String src, String originalFilename) throws Exception {
+        String path = FilePathUtils.wholePathName(src, originalFilename);
+        return writeToStore(bytes, path);
+    }
+}
