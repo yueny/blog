3ca555e3e3bb7092f0274a18212d7c063b40bafe
yueny <yueny09@163.com>
2019-08-29 19:50:15 +0800

    默认权限管理

--- web-backend/src/main/java/com/mtons/mblog/config/ShiroConfiguration.java
+++ web-backend/src/main/java/com/mtons/mblog/config/ShiroConfiguration.java
@@ -252,7 +252,7 @@ public class ShiroConfiguration {
         //.
 
         // 默认需要的权限
-//        hashMap.put("/admin/*", getAuthc(ShiroRuleFuncNames.adminImprove));
+        hashMap.put("/admin/*", "authc,perms[admin]");
 
         return hashMap;
     }
